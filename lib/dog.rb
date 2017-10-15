9c9af8dcc254bbb827612059272aa3c0b026bec6
Kayla Hansen <shiverr@gmail.com>
2017-10-15 22:23:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,15 @@
 class Dog
   def name=(dog_name) #instance variable
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
 
   def name
-    this_dogs_name #reading method
+    @this_dogs_name #reading method
   end
 
 end
 
 lassie = Dog.new
 lassie.name = "Lassie"
-lassie.name
+
+puts lassie.name
